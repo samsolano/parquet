40b105d3716486d46e9460307b5e97258b5b3bf6
Sam Solano <70192267+samsolano@users.noreply.github.com>
2025-04-20 14:54:56 -0700

    phony basic dashboard setup

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import * as React from "react"
+import { useState } from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+  const [mode ,setMode] = useState("dark")
+
+  return (
+        <Button variant="outline" size="icon" onClick={() => {
+
+            if (mode === "dark") {
+                setMode("light");
+                setTheme("light");
+            }
+            else {
+                setMode("dark");
+                setTheme("dark");
+            }
+        }}>
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+  )
+}
